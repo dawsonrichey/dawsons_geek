503c6983f65bc7e45fa2e4d14b3c1bef9a710c60
Dawson Richey <dawsonrichey@gmail.com>
2021-10-13 18:28:27 -0400

    re-ordered file structure and updated links

--- documentation.php
+++ documentation.php
@@ -4,16 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documents</title>
+    <title>Documentation</title>
+    <link rel="stylesheet" href="./static/css/global.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="./static/css/custom.css"/>
+    
+    <script type="javascript" src="./static/js/global.js"></script>
+    <script type="javascript" src="./static/js/custom_before.js"></script>
 </head>
 
 <body>
-<?php include('../../includes/global.php'); ?>
+<?php include('../dawsons_geek/static/php/global.php'); ?>
     <h1>Website/Project Documentation</h1>
+    CSS
 
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+JS
+
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+				
+    http://kenwheeler.github.io/slick/
     <p><i class="fa-solid fa-coffee fa-2xs"></i> When my six o’clock alarm buzzes, I require a pot of good java.</p>
   <p><i class="fa-solid fa-coffee fa-xs"></i> When my six o’clock alarm buzzes, I require a pot of good java.</p>
   <p><i class="fa-solid fa-coffee fa-sm"></i> When my six o’clock alarm buzzes, I require a pot of good java.</p>
@@ -22,20 +34,21 @@
   <p><i class="fa-solid fa-coffee fa-xl"></i> When my six o’clock alarm buzzes, I require a pot of good java.</p>
   <p><i class="fa-solid fa-coffee fa-2xl"></i> When my six o’clock alarm buzzes, I require a pot of good java.</p>
 
-Relative Sizing Class	Font Size	Equivalent in Pixels
-fa-2xs	0.625em	10px
-fa-xs	0.75em	12px
-fa-sm	0.875em	14px
-fa-lg	1.25em	20px
-fa-xl	1.5em	24px
-fa-2xl	2em	32px
 
     <p>
     https://fontawesome.com/v6.0/icons?s=solid%2Cbrands
 used cdnjs links https://cdnjs.com/</p>
 <p>Normalize.css
 
-</p>
 
+
+https://momentjs.com/?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library
+
+
+https://sortablejs.github.io/Sortable/
+https://sweetalert.js.org/?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library
+
+</p>
+<script type="javascript" src="./static/js/custom_after.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,18 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dawsons Geek | <?php echo 'here'; ?> </title>
+    <title>Dawsons Geek | Home Page </title>
+
+    <link rel="stylesheet" href="./static/css/global.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- https://pro.fontawesome.com/releases/v6.0.0-beta2/css/all.css -->
-    <!-- <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.0.0-beta2/css/all.css" /> -->
-    <!-- <link rel="stylesheet" href="./static/css/all-pro.css" /> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./static/css/custom.css"/>
+    
+    <script type="javascript" src="./static/js/global.js"></script>
+    <script type="javascript" src="./static/js/custom_before.js"></script>
 </head>
 <body>
-<?php include('./includes/global.php'); ?>
-    <h1><?php echo 'here'; ?></h1>
-    <!-- <i class="fa-solid fa-360-degrees"></i> -->
+<?php include('../dawsons_geek/static/php/global.php'); ?>
+    <h1>Dawson's Geek</h1>
     <i class="fa-solid fa-mask-face fa-xl"></i>
-    <!-- <i class="fa-solid fa-axe"></i> -->
+
+
+
+    <script type="javascript" src="./static/js/custom_after.js"></script>
 </body>
 </html>
